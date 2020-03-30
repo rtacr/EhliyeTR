939f73420ad2337bceb290bc6de0dbdf476c3c09
rtacr <32837894+rtacr@users.noreply.github.com>
2020-03-30 16:02:15 +0300

    Cleanup

--- lib/utils/constantValues.dart
+++
@@ -1,8 +0,0 @@
-const List<String> topicNames = [
-      "İlk Yardım",
-      "Trafik ve Çevre Bilgisi",
-      "Motor ve Araç Tekniği"
-    ];
-const String topic1 = "İlk Yardım";
-const String topic2 = "Trafik ve Çevre Bilgisi";
-const String topic3 = "Motor ve Araç Tekniği";
